052c8bb1b0c406754c3478192ed66d7c0000579b
Vadim Lapšin <vadim@nvp.ee>
2019-11-19 21:28:00 +0200

    Version 1.0

---
+++ StorJ Audit/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 26)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TextBox1.Size = New System.Drawing.Size(425, 632)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(474, 182)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Get Audits"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(474, 124)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(170, 22)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "14002"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(474, 59)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(170, 22)
+        Me.TextBox3.TabIndex = 3
+        Me.TextBox3.Text = "192.168.88.240"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(474, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 17)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Node IP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(474, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Node Port"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(474, 242)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(128, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Clear"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 685)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button2 As Button
+End Class
---
+++ StorJ Audit/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.IO
+Imports System.Net
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+            Dim list As New List(Of Object)
+            Dim satelite As HttpWebRequest
+            Dim sateliteresponce As HttpWebResponse = Nothing
+            Dim request As HttpWebRequest
+            Dim response As HttpWebResponse = Nothing
+            Dim reader As StreamReader
+
+            satelite = DirectCast(WebRequest.Create("http://" & TextBox3.Text & ":" & TextBox2.Text & "/api/dashboard"), HttpWebRequest)
+            sateliteresponce = DirectCast(satelite.GetResponse(), HttpWebResponse)
+            reader = New StreamReader(sateliteresponce.GetResponseStream())
+            Dim rawresp As String
+            rawresp = reader.ReadToEnd()
+            list.AddRange((JObject.Parse(rawresp)("data")("satellites")))
+
+            Dim egressCount As Long = 0
+            Dim ingressCount As Long = 0
+            Dim repairDownCount As Long = 0
+            Dim repairUpCount As Long = 0
+
+            For Each id As JObject In list
+                Dim obj As String = (id.GetValue("id"))
+                request = DirectCast(WebRequest.Create("http://" & TextBox3.Text & ":" & TextBox2.Text & "/api/satellite/" & obj), HttpWebRequest)
+                response = DirectCast(request.GetResponse(), HttpWebResponse)
+                reader = New StreamReader(response.GetResponseStream())
+                rawresp = reader.ReadToEnd()
+                TextBox1.Text = TextBox1.Text & ((JObject.Parse(rawresp)("data")("audit"))).ToString & vbNewLine
+
+
+                For Each values As Object In JsonConvert.DeserializeObject(Of List(Of Object))(JObject.Parse(rawresp)("data")("bandwidthDaily").ToString)
+                    Dim egressObject = values("egress")("usage")
+                    Dim ingressObject = values("ingress")("usage")
+                    Dim repairDownObject = values("ingress")("repair")
+                    Dim repairUpObject = values("egress")("repair")
+                    egressCount = egressCount + CLng(egressObject)
+                    ingressCount = ingressCount + CLng(ingressObject)
+                    repairDownCount = repairDownCount + CLng(repairDownObject)
+                    repairUpCount = repairUpCount + CLng(repairUpObject)
+
+                Next
+
+            Next
+            TextBox1.Text = TextBox1.Text & "Upload: " & Math.Round(egressCount / 1073741824, 2) & vbNewLine
+            TextBox1.Text = TextBox1.Text & "Download: " & Math.Round(ingressCount / 1073741824, 2) & vbNewLine
+            TextBox1.Text = TextBox1.Text & "Repair Download: " & Math.Round(repairDownCount / 1073741824, 2) & vbNewLine
+            TextBox1.Text = TextBox1.Text & "Repair Upload: " & Math.Round(repairUpCount / 1073741824, 3) & vbNewLine
+            TextBox1.Text = TextBox1.Text & "Total: " & Math.Round((repairDownCount + repairUpCount + ingressCount + egressCount) / 1073741824, 2) & vbNewLine
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TextBox1.Text = ""
+    End Sub
+End Class
+
+
