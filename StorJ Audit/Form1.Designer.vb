ef52060f0400315fcd456e9f27d34c63a0756e25
idsysltd <vadim.lapshin@idsys.ee>
2019-11-30 22:43:38 +0200

    Made posible to get info from lot of nodes

--- StorJ Audit/Form1.Designer.vb
+++ StorJ Audit/Form1.Designer.vb
@@ -22,53 +22,56 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Button2 = New System.Windows.Forms.Button()
+        Me.NodeList = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.NodeView = New System.Windows.Forms.DataGridView()
+        Me.Node = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Satellite = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Audits = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Uptime = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Upload = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Download = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.RepearUpload = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TotalBandwidth = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.NodeView, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 26)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox1.Size = New System.Drawing.Size(425, 632)
-        Me.TextBox1.TabIndex = 0
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(474, 182)
+        Me.Button1.Location = New System.Drawing.Point(1327, 535)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(128, 23)
+        Me.Button1.Size = New System.Drawing.Size(91, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Get Audits"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'TextBox2
         '
-        Me.TextBox2.Location = New System.Drawing.Point(474, 124)
+        Me.TextBox2.Location = New System.Drawing.Point(1327, 436)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(170, 22)
+        Me.TextBox2.Size = New System.Drawing.Size(133, 22)
         Me.TextBox2.TabIndex = 2
         Me.TextBox2.Text = "14002"
         '
         'TextBox3
         '
-        Me.TextBox3.Location = New System.Drawing.Point(474, 59)
+        Me.TextBox3.Location = New System.Drawing.Point(1327, 381)
         Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(170, 22)
+        Me.TextBox3.Size = New System.Drawing.Size(133, 22)
         Me.TextBox3.TabIndex = 3
         Me.TextBox3.Text = "192.168.88.240"
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(474, 26)
+        Me.Label1.Location = New System.Drawing.Point(1327, 361)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(58, 17)
         Me.Label1.TabIndex = 4
@@ -77,7 +80,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(474, 94)
+        Me.Label2.Location = New System.Drawing.Point(1327, 416)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(72, 17)
         Me.Label2.TabIndex = 5
@@ -85,37 +88,147 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(474, 242)
+        Me.Button2.Location = New System.Drawing.Point(1327, 564)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(128, 23)
+        Me.Button2.Size = New System.Drawing.Size(91, 23)
         Me.Button2.TabIndex = 6
         Me.Button2.Text = "Clear"
         Me.Button2.UseVisualStyleBackColor = True
+        '
+        'NodeList
+        '
+        Me.NodeList.FormattingEnabled = True
+        Me.NodeList.ItemHeight = 16
+        Me.NodeList.Location = New System.Drawing.Point(1327, 27)
+        Me.NodeList.Name = "NodeList"
+        Me.NodeList.Size = New System.Drawing.Size(188, 292)
+        Me.NodeList.TabIndex = 7
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(1327, 491)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(91, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Add Node"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(1424, 491)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(91, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Delete Node"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'NodeView
+        '
+        Me.NodeView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.NodeView.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Node, Me.Satellite, Me.Audits, Me.Uptime, Me.Upload, Me.Download, Me.RepearUpload, Me.TotalBandwidth})
+        Me.NodeView.Location = New System.Drawing.Point(1, 5)
+        Me.NodeView.Name = "NodeView"
+        Me.NodeView.RowHeadersWidth = 51
+        Me.NodeView.RowTemplate.Height = 24
+        Me.NodeView.Size = New System.Drawing.Size(1304, 678)
+        Me.NodeView.TabIndex = 10
+        '
+        'Node
+        '
+        Me.Node.HeaderText = "Node"
+        Me.Node.MinimumWidth = 6
+        Me.Node.Name = "Node"
+        Me.Node.ReadOnly = True
+        Me.Node.Width = 150
+        '
+        'Satellite
+        '
+        Me.Satellite.HeaderText = "Satellite"
+        Me.Satellite.MinimumWidth = 6
+        Me.Satellite.Name = "Satellite"
+        Me.Satellite.ReadOnly = True
+        Me.Satellite.Width = 175
+        '
+        'Audits
+        '
+        Me.Audits.HeaderText = "Audits"
+        Me.Audits.MinimumWidth = 6
+        Me.Audits.Name = "Audits"
+        Me.Audits.ReadOnly = True
+        '
+        'Uptime
+        '
+        Me.Uptime.HeaderText = "Uptime"
+        Me.Uptime.MinimumWidth = 6
+        Me.Uptime.Name = "Uptime"
+        Me.Uptime.ReadOnly = True
+        '
+        'Upload
+        '
+        Me.Upload.HeaderText = "Upload"
+        Me.Upload.MinimumWidth = 6
+        Me.Upload.Name = "Upload"
+        Me.Upload.ReadOnly = True
+        '
+        'Download
+        '
+        Me.Download.HeaderText = "Download"
+        Me.Download.MinimumWidth = 6
+        Me.Download.Name = "Download"
+        Me.Download.ReadOnly = True
+        '
+        'RepearUpload
+        '
+        Me.RepearUpload.HeaderText = "RepearUpload"
+        Me.RepearUpload.MinimumWidth = 6
+        Me.RepearUpload.Name = "RepearUpload"
+        Me.RepearUpload.ReadOnly = True
+        '
+        'TotalBandwidth
+        '
+        Me.TotalBandwidth.HeaderText = "Total Bandwidth"
+        Me.TotalBandwidth.MinimumWidth = 6
+        Me.TotalBandwidth.Name = "TotalBandwidth"
+        Me.TotalBandwidth.ReadOnly = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 685)
+        Me.ClientSize = New System.Drawing.Size(1527, 685)
+        Me.Controls.Add(Me.NodeView)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.NodeList)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Node values Agregator"
+        CType(Me.NodeView, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents TextBox1 As TextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Button2 As Button
+    Friend WithEvents NodeList As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents NodeView As DataGridView
+    Friend WithEvents Node As DataGridViewTextBoxColumn
+    Friend WithEvents Satellite As DataGridViewTextBoxColumn
+    Friend WithEvents Audits As DataGridViewTextBoxColumn
+    Friend WithEvents Uptime As DataGridViewTextBoxColumn
+    Friend WithEvents Upload As DataGridViewTextBoxColumn
+    Friend WithEvents Download As DataGridViewTextBoxColumn
+    Friend WithEvents RepearUpload As DataGridViewTextBoxColumn
+    Friend WithEvents TotalBandwidth As DataGridViewTextBoxColumn
 End Class
--- StorJ Audit/Form1.vb
+++ StorJ Audit/Form1.vb
@@ -14,58 +14,116 @@ Public Class Form1
             Dim response As HttpWebResponse = Nothing
             Dim reader As StreamReader
 
-            satelite = DirectCast(WebRequest.Create("http://" & TextBox3.Text & ":" & TextBox2.Text & "/api/dashboard"), HttpWebRequest)
+            satelite = DirectCast(WebRequest.Create("http://" & NodeList.Items(0) & "/api/dashboard"), HttpWebRequest)
             sateliteresponce = DirectCast(satelite.GetResponse(), HttpWebResponse)
             reader = New StreamReader(sateliteresponce.GetResponseStream())
             Dim rawresp As String
             rawresp = reader.ReadToEnd()
             list.AddRange((JObject.Parse(rawresp)("data")("satellites")))
 
-            Dim egressCount As Long = 0
-            Dim ingressCount As Long = 0
-            Dim repairDownCount As Long = 0
-            Dim repairUpCount As Long = 0
-
-            For Each id As JObject In list
-
-                Dim obj As String = (id.GetValue("id"))
-                TextBox1.Text = TextBox1.Text & " Setelite ID " & obj & vbNewLine
-                request = DirectCast(WebRequest.Create("http://" & TextBox3.Text & ":" & TextBox2.Text & "/api/satellite/" & obj), HttpWebRequest)
-                response = DirectCast(request.GetResponse(), HttpWebResponse)
-                reader = New StreamReader(response.GetResponseStream())
-                rawresp = reader.ReadToEnd()
-                TextBox1.Text = TextBox1.Text & "Audit" & vbNewLine
-                TextBox1.Text = TextBox1.Text & ((JObject.Parse(rawresp)("data")("audit"))).ToString & vbNewLine
-                TextBox1.Text = TextBox1.Text & "Uptime" & vbNewLine
-                TextBox1.Text = TextBox1.Text & ((JObject.Parse(rawresp)("data")("uptime"))).ToString & vbNewLine
+            Dim TotalegressCount As Long = 0
+            Dim TotalingressCount As Long = 0
+            Dim TotalrepairDownCount As Long = 0
+            Dim TotalrepairUpCount As Long = 0
+            For Each node As String In NodeList.Items
 
 
-                For Each values As Object In JsonConvert.DeserializeObject(Of List(Of Object))(JObject.Parse(rawresp)("data")("bandwidthDaily").ToString)
-                    Dim egressObject = values("egress")("usage")
-                    Dim ingressObject = values("ingress")("usage")
-                    Dim repairDownObject = values("ingress")("repair")
-                    Dim repairUpObject = values("egress")("repair")
-                    egressCount = egressCount + CLng(egressObject)
-                    ingressCount = ingressCount + CLng(ingressObject)
-                    repairDownCount = repairDownCount + CLng(repairDownObject)
-                    repairUpCount = repairUpCount + CLng(repairUpObject)
+
+                Dim egressCount As Long = 0
+                Dim ingressCount As Long = 0
+                Dim repairDownCount As Long = 0
+                Dim repairUpCount As Long = 0
+
+                Dim NodeegressCount As Long = 0
+                Dim NodeingressCount As Long = 0
+                Dim NoderepairDownCount As Long = 0
+                Dim NoderepairUpCount As Long = 0
+
+                For Each id As JObject In list
+                    egressCount = 0
+                    ingressCount = 0
+                    repairDownCount = 0
+                    repairUpCount = 0
+                    Dim obj As String = (id.GetValue("id"))
+
+                    request = DirectCast(WebRequest.Create("http://" & node & "/api/satellite/" & obj), HttpWebRequest)
+                    response = DirectCast(request.GetResponse(), HttpWebResponse)
+                    reader = New StreamReader(response.GetResponseStream())
+                    rawresp = reader.ReadToEnd()
+
+                    Dim Audits = ((JObject.Parse(rawresp)("data")("audit")("successCount"))).ToString
+                    Dim TotalAudits = ((JObject.Parse(rawresp)("data")("audit")("totalCount"))).ToString
+
+                    Dim Uptime = ((JObject.Parse(rawresp)("data")("uptime")("successCount"))).ToString
+                    Dim TotalUptime = ((JObject.Parse(rawresp)("data")("uptime")("totalCount"))).ToString
+                    Try
+
+
+                        For Each values As Object In JsonConvert.DeserializeObject(Of List(Of Object))(JObject.Parse(rawresp)("data")("bandwidthDaily").ToString)
+                            Dim egressObject = values("egress")("usage")
+                            Dim ingressObject = values("ingress")("usage")
+                            Dim repairDownObject = values("ingress")("repair")
+                            Dim repairUpObject = values("egress")("repair")
+                            egressCount = egressCount + CLng(egressObject)
+                            ingressCount = ingressCount + CLng(ingressObject)
+                            repairDownCount = repairDownCount + CLng(repairDownObject)
+                            repairUpCount = repairUpCount + CLng(repairUpObject)
+
+                        Next
+                    Catch ex As Exception
+
+                    End Try
+                    NodeegressCount = NodeegressCount + egressCount
+                    NodeingressCount = NodeingressCount + ingressCount
+                    NoderepairDownCount = NoderepairDownCount + repairDownCount
+                    NoderepairUpCount = NoderepairUpCount + repairUpCount
+                    NodeView.Rows.Add({node, id.GetValue("url"), Audits & "/" & TotalAudits, Uptime & "/" & TotalUptime, Math.Round(egressCount / 1000000000, 2), Math.Round(ingressCount / 1000000000, 2), Math.Round(repairUpCount / 1000000000, 3), Math.Round((repairDownCount + repairUpCount + ingressCount + egressCount) / 1000000000, 2)})
 
                 Next
-
+                TotalegressCount = TotalegressCount + NodeegressCount
+                TotalingressCount = TotalingressCount + NodeingressCount
+                TotalrepairDownCount = TotalrepairDownCount + NoderepairDownCount
+                TotalrepairUpCount = TotalrepairUpCount + NoderepairUpCount
+                NodeView.Rows.Add({"Node Total", "", "", "", Math.Round(NodeegressCount / 1000000000, 2), Math.Round(NodeingressCount / 1000000000, 2), Math.Round(NoderepairUpCount / 1000000000, 2), Math.Round((NodeegressCount + NodeingressCount + NoderepairUpCount + NoderepairDownCount) / 1000000000, 2)})
             Next
-            TextBox1.Text = TextBox1.Text & "Upload: " & Math.Round(egressCount / 1000000000, 2) & vbNewLine
-            TextBox1.Text = TextBox1.Text & "Download: " & Math.Round(ingressCount / 1000000000, 2) & vbNewLine
-            TextBox1.Text = TextBox1.Text & "Repair Download: " & Math.Round(repairDownCount / 1000000000, 2) & vbNewLine
-            TextBox1.Text = TextBox1.Text & "Repair Upload: " & Math.Round(repairUpCount / 1000000000, 3) & vbNewLine
-            TextBox1.Text = TextBox1.Text & "Total: " & Math.Round((repairDownCount + repairUpCount + ingressCount + egressCount) / 1000000000, 2) & vbNewLine
+            NodeView.Rows.Add({"All Total", "", "", "", Math.Round(TotalegressCount / 1000000000, 2), Math.Round(TotalingressCount / 1000000000, 2), Math.Round(TotalrepairUpCount / 1000000000, 2), Math.Round((TotalegressCount + TotalingressCount + TotalrepairDownCount + TotalrepairUpCount) / 1000000000, 2)})
         Catch ex As Exception
             MsgBox(ex.ToString)
         End Try
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        TextBox1.Text = ""
+        NodeView.Rows.Clear()
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim list() As String = My.Settings.NodeList.Split(",")
+
+        For Each item As String In list
+            If item <> "" Then NodeList.Items.Add(item)
+        Next
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Not NodeList.Items.Contains(TextBox3.Text & ":" & TextBox2.Text) Then
+            NodeList.Items.Add(TextBox3.Text & ":" & TextBox2.Text)
+            My.Settings.NodeList = My.Settings.NodeList & "," & TextBox3.Text & ":" & TextBox2.Text
+            My.Settings.Save()
+        Else
+            MsgBox("Node Exists in the list")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        NodeList.Items.Remove(NodeList.SelectedItem)
+        My.Settings.NodeList = ""
+        For Each nodeitem As String In NodeList.Items
+            My.Settings.NodeList = My.Settings.NodeList & nodeitem & ","
+        Next
+        My.Settings.Save()
+    End Sub
+
+
 End Class
 
 
